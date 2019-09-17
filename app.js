8be94e81703ed980d2cc855785aa93f56cc3df4e
Xavyr Moss <xavyr.moss@cybergrx.com>
2019-09-17 14:59:35 -0700

    change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 // [START hello_world]
 // Say hello!
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hellasfdsfdgfhjhdfasdfo Xavyr" });
+  res.status(200).json({ message: " Xavyr" });
 });
 // [END hello_world]
 
